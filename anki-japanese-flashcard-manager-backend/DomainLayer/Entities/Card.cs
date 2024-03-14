a0c0ae09661fce78aab1d5049f6c0a27778962d0
Tyler Mason <134988352+TMason11095@users.noreply.github.com>
2024-03-13 22:10:57 -0500

    Copied over DomainLayer code from main repo.

---
+++ anki-japanese-flashcard-manager-backend/DomainLayer/Entities/Card.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace anki_japanese_flashcard_manager_backend.DomainLayer.Entities
+{
+	public class Card
+	{
+		public long Id { get; protected set; }
+		[Column("nid")]
+		public long NoteId { get; protected set; }
+		[Column("did")]
+		public long DeckId { get; set; }
+		[Column("ivl")]
+		public int Interval { get; protected set; }
+
+		//Override Equals
+		public override bool Equals(object obj)
+		{
+			//Fail if not Card
+			if (obj is null || !(obj is Card otherCard))
+			{
+				return false;
+			}
+
+			return Id == otherCard.Id
+				&& NoteId == otherCard.NoteId
+				&& DeckId == otherCard.DeckId
+				&& Interval == otherCard.Interval;
+		}
+		//Override GetHashCode
+		public override int GetHashCode()
+		{
+			return HashCode.Combine(Id, NoteId, DeckId, Interval);
+		}
+	}
+}
---
+++ anki-japanese-flashcard-manager-backend/DomainLayer/Entities/Note.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace anki_japanese_flashcard_manager_backend.DomainLayer.Entities
+{
+	public class Note
+	{
+		public long Id { get; protected set; }
+		public string Tags { get; protected set; }
+		[NotMapped] //Convert space delimited Tags field to list
+		public List<string> TagsList { get { return Tags.Split(' ').ToList(); } }
+		[Column("flds")]
+		public string Fields { get; protected set; }
+		[Column("sfld")]
+		public string SortField { get; protected set; }
+	}
+}
