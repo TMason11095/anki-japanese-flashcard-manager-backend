5edc8fb61b92279418d72ec39c7edaec2c047ec4
Tyler Mason <134988352+TMason11095@users.noreply.github.com>
2024-03-14 21:47:30 -0500

    Added a CompositionRoot.

---
+++ anki-japanese-flashcard-manager-backend/Composition/CompositionRoot.cs
@@ -0,0 +1,29 @@
+﻿using anki_japanese_flashcard_manager_backend.ApplicationLayer.Interfaces.Services.Managements;
+using anki_japanese_flashcard_manager_backend.ApplicationLayer.Interfaces.Services;
+using anki_japanese_flashcard_manager_backend.ApplicationLayer.Services.Managements;
+using anki_japanese_flashcard_manager_backend.ApplicationLayer.Services;
+using anki_japanese_flashcard_manager_backend.DataAccessLayer.Contexts;
+using anki_japanese_flashcard_manager_backend.DataAccessLayer.Interfaces.Contexts;
+using anki_japanese_flashcard_manager_backend.DataAccessLayer.Repositories;
+using anki_japanese_flashcard_manager_backend.DomainLayer.Interfaces.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace anki_japanese_flashcard_manager_backend.Composition
+{
+	public static class CompositionRoot
+	{
+		public static void Compose(IServiceCollection services)
+		{
+			//Register the Data Access Layer
+			services.AddScoped<IAnki2Context, Anki2Context>();
+			services.AddScoped<ICardRepository, CardRepository>();
+			services.AddScoped<IDeckRepository, DeckRepository>();
+			//Register the Application Layer
+			services.AddScoped<IDeckService, DeckService>();
+			services.AddScoped<IKanjiCardService, KanjiCardService>();
+			services.AddScoped<IKanjiDeckService, KanjiDeckService>();
+			services.AddScoped<IKanjiNoteService, KanjiNoteService>();
+			services.AddScoped<IKanjiServiceManagement, KanjiServiceManagement>();
+		}
+	}
+}
